b76150377fa8a22b790efc7822dd98725e0972cf
Simon Dalvai <info@simondalvai.org>
2024-10-20 23:35:17 +0200

    nvim: remove nvim-cmp and use built-in autocompletition instead

--- nvim/init.lua
+++ nvim/init.lua
@@ -33,13 +33,13 @@ Plug('neovim/nvim-lspconfig')
 Plug('mfussenegger/nvim-dap')
 Plug('williamboman/mason.nvim')
 -- auto complete
-Plug('hrsh7th/cmp-nvim-lsp')
-Plug('hrsh7th/cmp-buffer')
-Plug('hrsh7th/cmp-path')
-Plug('hrsh7th/cmp-cmdline')
-Plug('hrsh7th/nvim-cmp')
-Plug('L3MON4D3/LuaSnip')
-Plug('saadparwaiz1/cmp_luasnip')
+-- Plug('hrsh7th/cmp-nvim-lsp')
+-- Plug('hrsh7th/cmp-buffer')
+-- Plug('hrsh7th/cmp-path')
+-- Plug('hrsh7th/cmp-cmdline')
+-- Plug('hrsh7th/nvim-cmp')
+-- Plug('L3MON4D3/LuaSnip')
+-- Plug('saadparwaiz1/cmp_luasnip')
 
 vim.call('plug#end')
 
@@ -213,49 +213,3 @@ end)
 
 require("ibl").setup { indent = { highlight = highlight } }
 
--- ----------------------
--- nvim-cmp
--- ----------------------
-local cmp = require'cmp'
-
-cmp.setup({
-snippet = {
-  -- REQUIRED - you must specify a snippet engine
-  expand = function(args)
-    require('luasnip').lsp_expand(args.body)
-    vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-  end,
-},
-window = {
-  -- completion = cmp.config.window.bordered(),
-  -- documentation = cmp.config.window.bordered(),
-},
-mapping = cmp.mapping.preset.insert({
-  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-  ['<C-f>'] = cmp.mapping.scroll_docs(4),
-  ['<C-Space>'] = cmp.mapping.complete(),
-  ['<C-e>'] = cmp.mapping.abort(),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-}),
-sources = cmp.config.sources({
-  { name = 'nvim_lsp' },
-  { name = 'luasnip' },
-}, {
-  { name = 'buffer' },
-})
-})
-
--- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline({ '/', '?' }, {
-mapping = cmp.mapping.preset.cmdline(),
-sources = {
-  { name = 'buffer' }
-}
-})
-
--- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['gdscript'].setup {
-capabilities = capabilities
-}
