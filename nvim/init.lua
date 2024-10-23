e13e758dc67564498573993eb6ec418483ca5533
Simon Dalvai <info@simondalvai.org>
2024-10-23 13:41:32 +0200

    nvim: remove commented nvim-cmp; add gdresource dap config

--- nvim/init.lua
+++ nvim/init.lua
@@ -32,14 +32,6 @@ Plug('lukas-reineke/indent-blankline.nvim')
 Plug('neovim/nvim-lspconfig')
 Plug('mfussenegger/nvim-dap')
 Plug('williamboman/mason.nvim')
--- auto complete
--- Plug('hrsh7th/cmp-nvim-lsp')
--- Plug('hrsh7th/cmp-buffer')
--- Plug('hrsh7th/cmp-path')
--- Plug('hrsh7th/cmp-cmdline')
--- Plug('hrsh7th/nvim-cmp')
--- Plug('L3MON4D3/LuaSnip')
--- Plug('saadparwaiz1/cmp_luasnip')
 
 vim.call('plug#end')
 
@@ -184,6 +176,16 @@ dap.configurations.gdscript = {
     type = "godot",
     request = "launch",
     name = "Launch scene",
+    project = "${workspaceFolder}",
+  }
+}
+
+dap.configurations.gdresource = {
+  {
+    type = "godot",
+    request = "launch",
+    name = "Launch current scene",
+    scene = "pinned",
     project = "${workspaceFolder}",
   }
 }
