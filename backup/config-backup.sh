5f7db99c21fab014cbc21c9e2b3c6615439fabe4
Simon Dalvai <info@simondalvai.org>
2025-04-28 10:07:55 +0200

    backup/

--- backup/config-backup.sh
+++ backup/config-backup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-config_path="/home/dulvui/sync/computer/config"
+home_path="/home/dulvui"
+config_path="$home_path/sync/computer/config"
 
 # used for files or directories, that don't work with soft links
 # like godot's config
-echo "start backup at $(date)" > config-backup.log
+echo "start backup at $(date)" > $home_path/logs/config-backup.log
 
 # iterate over directories and execute backup.sh
 # .*/ adds hidden directories
@@ -12,10 +13,10 @@ for dir in $(ls -d $config_path/*/ $config_path/.*/);
 do
     if [ -f $dir/backup.sh ];
     then
-        echo "backing up $dir...\n" >> config-backup.log
+        echo "backing up $dir...\n" >> $home_path/logs/config-backup.log
         bash $dir/backup.sh;
-        echo "backing up $dir done.\n" >> config-backup.log
+        echo "backing up $dir done.\n" >> $home_path/logs/config-backup.log
     fi;
 done
 
-echo "end backup at $(date)" >> config-backup.log
+echo "end backup at $(date)" >> $home_path/logs/config-backup.log
--- backup/disk-backup.sh
+++ backup/disk-backup.sh
@@ -13,35 +13,36 @@ config_path="$home_path/sync/computer/config"
 
 function backup() {
     if [ $# -ne 2 ]; then
-        echo "2 arguments needed: dir-file, destination" >> disk-backup.log
+        echo "2 arguments needed: dir-file, destination" >> $home_path/logs/disk-backup.log
         return 1
     fi
 
     if [ ! -d "$2" ]; then
-        echo "$2 not mounted, skipping..." >> disk-backup.log
+        echo "$2 not mounted, skipping..." >> $home_path/logs/disk-backup.log
         return 0
     fi
 
 
     while read dir; do
-        echo "backing up $2$dir..." >> disk-backup.log
+        echo "backing up $2$dir..." >> $home_path/logs/disk-backup.log
         rsync -az --delete --exclude ".stversions" "$home_path/$dir" $2$dir
-        echo "backing up $2$dir done." >> disk-backup.log
+        echo "backing up $2$dir done." >> $home_path/logs/disk-backup.log
     done <"$config_path/backup/$1"
 }
 
-echo "start backup at $(date)" > disk-backup.log
+echo "start backup at $(date)" > $home_path/logs/disk-backup.log
 
-echo "backup to sd lexar..." >> disk-backup.log
-backup "dirs-full.txt" "/media/dulvui/18d8f048-1239-46ca-8548-18b709fc2c0d/"
-echo "backup to sd lexar done." >> disk-backup.log
+# echo "backup to sd lexar..." >> $home_path/logs/disk-backup.log
+# backup "dirs-full.txt" "/media/dulvui/18d8f048-1239-46ca-8548-18b709fc2c0d/"
+# echo "backup to sd lexar done." >> $home_path/logs/disk-backup.log
 
-echo "backup to sd samsung..." >> disk-backup.log
+echo "backup to sd samsung..." >> $home_path/logs/disk-backup.log
 backup "dirs-tiny.txt" "/media/dulvui/63add787-ae09-465c-9d7e-be3e16b346d1/"
-echo "backup to sd samsung done." >> disk-backup.log
+echo "backup to sd samsung done." >> $home_path/logs/disk-backup.log
 
-echo "backup to ssd..." >> disk-backup.log
+echo "backup to ssd..." >> $home_path/logs/disk-backup.log
 backup "dirs-full.txt" "/media/dulvui/24ecc7ab-d81d-47b1-bc9d-e207d72cbffc/"
-echo "backup to ssd done." >> disk-backup.log
+echo "backup to ssd done." >> $home_path/logs/disk-backup.log
 
-echo "end backup at $(date)" >> disk-backup.log
+echo "end backup at $(date)" >> $home_path/logs/disk-backup.log
+
